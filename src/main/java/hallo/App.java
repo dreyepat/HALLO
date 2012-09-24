ae422584880b7235d72e1276a07e18f34e192f2b
dreyepat <titz@gmx.ch>
2012-09-24 06:10:00 +0200

    erste Aenderungen

--- src/main/java/hallo/App.java
+++ src/main/java/hallo/App.java
@@ -1,13 +1,13 @@
 package hallo;
 
 /**
- * Hello world!
+ * Hallo Welt!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hallo Welt!" );
     }
 }
